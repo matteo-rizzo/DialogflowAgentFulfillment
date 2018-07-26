e624fa46f87634730853a3fc00c846515c476f03
Matte <matteo_rizzo96@hotmail.it>
2018-07-26 14:53:01 +0200

    Set up a few more tests for the fulfillment

---
+++ firebaseFulfillment/firebase/functions/test/apicaller.test.js
@@ -0,0 +1,119 @@
+const APIcaller = require('../apicaller');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const SearchParameters = require('../searchparameters');
+const chai = require('chai');
+const assert = chai.assert;
+
+describe('APIcaller testing', () => {
+        
+    // Mock http request and response
+    const request = {
+        "body": {
+            "responseId": "1b460276-3247-4ad8-a78a-86d944723a99",
+            "session": "projects/your-agents-project-id/agent/sessions/335fa429-102d-c0ab-a4da-2a04f210c63b",
+            "queryResult": {
+                "queryText": "scarpe da calcio da uomo",
+                "parameters": {
+                "sport": "",
+                "macrocategory": "scarpe",
+                "color": [],
+                "accessories": "",
+                "shoes": "Calcio",
+                "clothing": "",
+                "gender": "uomo"
+                },
+                "allRequiredParamsPresent": true,
+                "fulfillmentMessages": [],
+                "outputContexts": [
+                {
+                    "name": "projects/your-agents-project-id/agent/sessions/335fa429-102d-c0ab-a4da-2a04f210c63b/contexts/productsearch",
+                    "lifespanCount": 20,
+                    "parameters": {
+                    "clothing": "",
+                    "accessories": "",
+                    "color.original": "",
+                    "sport.original": "",
+                    "sport": "",
+                    "macrocategory": "scarpe",
+                    "color": [],
+                    "shoes": "Calcio",
+                    "shoes.original": "calcio",
+                    "gender": "uomo",
+                    "macrocategory.original": "scarpe",
+                    "accessories.original": "",
+                    "gender.original": "uomo",
+                    "clothing.original": ""
+                    }
+                },
+                {
+                    "name": "projects/your-agents-project-id/agent/sessions/335fa429-102d-c0ab-a4da-2a04f210c63b/contexts/productsearch-followup",
+                    "lifespanCount": 10,
+                    "parameters": {
+                    "sport.original": "",
+                    "sport": "",
+                    "macrocategory": "scarpe",
+                    "color": [],
+                    "shoes.original": "calcio",
+                    "shoes": "Calcio",
+                    "gender": "uomo",
+                    "macrocategory.original": "scarpe",
+                    "accessories.original": "",
+                    "gender.original": "uomo",
+                    "clothing.original": "",
+                    "clothing": "",
+                    "accessories": "",
+                    "color.original": ""
+                    }
+                }
+                ],
+                "intent": {
+                "name": "projects/conversational-ui-207612/agent/intents/df25baf2-5ab0-4e48-b69a-1fd297cd7bd5",
+                "displayName": "productSearch"
+                },
+                "intentDetectionConfidence": 1,
+                "diagnosticInfo": {
+                "webhook_latency_ms": 4203
+                },
+                "languageCode": "it"
+            },
+            "webhookStatus": {
+                "message": "Webhook execution successful"
+            }
+        }
+    };
+    const response = {};
+
+    // Mock agent
+    global.agent = new WebhookClient({ request, response });
+    
+    // Construct search parameters
+    const searchParameters = new SearchParameters();
+
+    it('Should call the categories API', () => {
+
+        return APIcaller.callCategoriesApi('uomo-scarpe').then((categories) => {
+            return assert.isNotEmpty(categories);
+        }).catch((err) => {
+            throw err;
+        })
+    })
+
+    it('Should call the product search API', () => {
+
+        return APIcaller.callProductSearchApi(searchParameters).then((products) => {
+            return assert.isNotEmpty(products);
+        }).catch((err) => {
+            throw err;
+        })
+    })
+
+    it('Should call the products API', () => {
+
+        return APIcaller.callProductsApi('101.173266_C4732').then((product) => {
+            return assert.isNotEmpty(product);
+        }).catch((err) => {
+            throw err;
+        })
+    })
+      
+})
---
+++ firebaseFulfillment/firebase/functions/test/searchparameters.test.js
@@ -0,0 +1,143 @@
+const {WebhookClient} = require('dialogflow-fulfillment');
+const SearchParameters = require('../searchparameters');
+const chai = require('chai');
+const assert = chai.assert;
+
+describe('SearchParameters testing', () => {
+        
+    // Mock http request and response
+    const request = {
+      "body": {
+          "responseId": "1b460276-3247-4ad8-a78a-86d944723a99",
+          "session": "projects/your-agents-project-id/agent/sessions/335fa429-102d-c0ab-a4da-2a04f210c63b",
+          "queryResult": {
+            "queryText": "scarpe da calcio da uomo",
+            "parameters": {
+              "sport": "",
+              "macrocategory": "scarpe",
+              "color": [],
+              "accessories": "",
+              "shoes": "Calcio",
+              "clothing": "",
+              "gender": "uomo"
+            },
+            "allRequiredParamsPresent": true,
+            "fulfillmentMessages": [],
+            "outputContexts": [
+              {
+                "name": "projects/your-agents-project-id/agent/sessions/335fa429-102d-c0ab-a4da-2a04f210c63b/contexts/productsearch",
+                "lifespanCount": 20,
+                "parameters": {
+                  "clothing": "",
+                  "accessories": "",
+                  "color.original": "",
+                  "sport.original": "",
+                  "sport": "",
+                  "macrocategory": "scarpe",
+                  "color": [],
+                  "shoes": "Calcio",
+                  "shoes.original": "calcio",
+                  "gender": "uomo",
+                  "macrocategory.original": "scarpe",
+                  "accessories.original": "",
+                  "gender.original": "uomo",
+                  "clothing.original": ""
+                }
+              },
+              {
+                "name": "projects/your-agents-project-id/agent/sessions/335fa429-102d-c0ab-a4da-2a04f210c63b/contexts/productsearch-followup",
+                "lifespanCount": 10,
+                "parameters": {
+                  "sport.original": "",
+                  "sport": "",
+                  "macrocategory": "scarpe",
+                  "color": [],
+                  "shoes.original": "calcio",
+                  "shoes": "Calcio",
+                  "gender": "uomo",
+                  "macrocategory.original": "scarpe",
+                  "accessories.original": "",
+                  "gender.original": "uomo",
+                  "clothing.original": "",
+                  "clothing": "",
+                  "accessories": "",
+                  "color.original": ""
+                }
+              }
+            ],
+            "intent": {
+              "name": "projects/conversational-ui-207612/agent/intents/df25baf2-5ab0-4e48-b69a-1fd297cd7bd5",
+              "displayName": "productSearch"
+            },
+            "intentDetectionConfidence": 1,
+            "diagnosticInfo": {
+              "webhook_latency_ms": 4203
+            },
+            "languageCode": "it"
+          },
+          "webhookStatus": {
+            "message": "Webhook execution successful"
+          }
+        }
+      };
+    const response = {};
+
+    // Mock agent
+    global.agent = new WebhookClient({ request, response });
+    
+    // Construct search parameters
+    const searchParameters = new SearchParameters();
+
+    it('Should construct the search parameters', () => {
+
+        return assert.exists(searchParameters);
+    })
+
+    it('Should construct the category map', () => {
+
+      const parameters = {
+          "shoes": "Heritage",
+          "clothing": "Felpe",
+          "accessories": "Berretti",
+          "sport": "Tennis"
+      };
+
+      const expectedMap = new Map([
+        ["scarpe", "heritage"],
+        ["abbigliamento", "felpe"],
+        ["accessori", "berretti"],
+        ["sport", "tennis"]
+      ]);
+
+      return assert.deepEqual(searchParameters.getCategoryMap(parameters), expectedMap);
+    })
+
+    it('Should construct the category names map', () => {
+
+      const expectedMap = new Map([
+        ["scarpe", "shoes"],
+        ["abbigliamento", "clothing"],
+        ["accessori", "accessories"],
+        ["sport", "sport"]
+    ]);
+
+      return assert.deepEqual(searchParameters.getCategoryNamesMap(), expectedMap);
+    })
+
+    it('Should return the right macrocategory given a category', () => {
+
+        const parameters = {
+            "shoes": "Calcio",
+            "clothing": "Felpe",
+            "accessories": "Berretti",
+            "sport": "Tennis"
+        }
+
+        return assert.strictEqual(searchParameters.getMacrocategoryByCategory(parameters), 'scarpe');
+    })
+      
+    it('Should return the query parameters', () => {
+
+        return assert.isNotEmpty(searchParameters.getQuery());
+    })
+})
